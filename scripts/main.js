b99226d0e92dec6f2c7f7acf10d1170f9bd761bb
Zhiyi Xu <yzxu@ucdavis.edu>
2018-07-06 12:05:58 -0700

    mainpage layout

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function hide(idname) {
+    var x = document.getElementById(idname);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+hide("topMenu")
