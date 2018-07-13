d2f520731efcae763996473312cb41bc39976b20
Maya Ram <mayaram@usc.edu>
2018-07-12 17:05:45 -0700

    summary

--- scripts/summaryMain.js
+++ scripts/summaryMain.js
@@ -1,3 +1,61 @@
+$(function() {
+	var tickers = ["Maya", "Alex", "Hung-Wei"];
+	var weights = [10, 20, 50];
+
+	var portfolioWeightsDataset = [];
+	for(var i = 0; i < tickers.length; i++) {
+		if(companyInfo[tickers[i]]) {
+			var cap = {};
+			cap.label = tickers[i];
+			cap.count = weights[i];
+			portfolioWeightsDataset.push(cap);
+		}
+	}
+
+	var tagsFrequencyDataset = [];
+	var dataFreq = getTagFrequency();
+	for (var f in dataFreq) {
+	    if (dataFreq.hasOwnProperty(f)) {
+	    	tagsFrequencyDataset.push(dataFreq[f] * 10);
+	    }
+	}
+	tagsFrequencyDataset.sort();
+
+	drawPie(portfolioWeightsDataset);
+	drawBarChart(tagsFrequencyDataset);
+
+
+
+	 /* chart list */
+	 for(var i = 0; i < tickers.length; i++) {
+	 	var weight = "weight_" + tickers[i];
+	 	$("#companyList").append("<tr><td class='ticker'>"+tickers[i]+"</td><td><input class='weight' type='text' name='" + tickers[i] + "' /></td></tr>");
+	 	
+	 
+	 }
+
+	 $('.weight').keyup(function(e) {
+	 		for(var x = 0; x < dataset.length; x++) {
+	 			var temp = dataset[x];
+	 			console.log($(this).attr('name'));
+	 			if(temp.label === $(this).attr('name')) {
+	 				console.log("here");
+	 				temp.count = $(this).val();
+	 			}
+	 		}
+
+    		$("#tagList").html($(this).val());
+    		drawPie(dataset)
+	 	});
+
+
+});
+
+function createPortfolioWeightTable() {
+
+
+}
+
 function cartOnClick(){
   location.href = "summary.html";
 }
@@ -41,48 +99,39 @@ function drawPie(dataset){
 
 }
 
-$(function() {
-	var tickers = ["Maya", "Alex", "Hung-Wei"];
-	var dataset = [];
-	for(var i = 0; i < tickers.length; i++) {
-		if(companyInfo[tickers[i]]) {
-			var cap = {};
-			cap.label = tickers[i];
-			cap.count = companyInfo[tickers[i]].marketCap;
-			dataset.push(cap);
-		}
-	}
+function drawBarChart(dataArray) {
+	
 
-	drawPie(dataset);
+// Create variable for the SVG
+var svg = d3.select("#tagChart").append("svg")
+          .attr("height","100%")
+          .attr("width","100%");
 
+// Select, append to SVG, and add attributes to rectangles for bar chart
+svg.selectAll("rect")
+    .data(dataArray)
+    .enter().append("rect")
+          .attr("class", "bar")
+          .attr("height", function(d, i) {return (d * 10)})
+          .attr("width","40")
+          .attr("x", function(d, i) {return (i * 60) + 25})
+          .attr("y", function(d, i) {return 400 - (d * 10)});
 
-	 /* tags pie chart */
-
-
-	 /* chart list */
-	 for(var i = 0; i < tickers.length; i++) {
-	 	var weight = "weight_" + tickers[i];
-	 	$("#companyList").append("<tr><td>"+tickers[i]+"</td><td><input class='weight' type='text' name='" + tickers[i] + "' /></td></tr>");
-	 	
-	 
-	 }
-
-	 $('.weight').keyup(function(e) {
-	 		for(var x = 0; x < dataset.length; x++) {
-	 			var temp = dataset[x];
-	 			console.log($(this).attr('name'));
-	 			if(temp.label === $(this).attr('name')) {
-	 				console.log("here");
-	 				temp.count = $(this).val();
-	 			}
-	 		}
-
-    		$("#tagList").html($(this).val());
-    		drawPie(dataset)
-	 	});
+// Select, append to SVG, and add attributes to text
+svg.selectAll("text")
+    .data(dataArray)
+    .enter().append("text")
+    .text(function(d) {return d})
+           .attr("class", "text")
+           .attr("x", function(d, i) {return (i * 60) + 36})
+           .attr("y", function(d, i) {return 415 - (d * 10)});
 
 
-});
+}
+
+
+
+
 
 
 document.getElementById("right-brand").style.cursor = "pointer";
