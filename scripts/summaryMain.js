8cc3eb30a860a059f0e861aab5dbca9498433cfb
Zhiyi Xu <yzxu@ucdavis.edu>
2018-07-12 20:04:45 -0700

    donut

--- scripts/summaryMain.js
+++ scripts/summaryMain.js
@@ -1,5 +1,4 @@
 $(function() {
-	console.log("hello");
 	var tickers = ["AMZN", "IBM", "JPM"];
 	var weights = [10, 20, 50];
 
@@ -15,30 +14,26 @@ $(function() {
 
 	var tagsFrequencyDataset = [];
 	var dataFreq = getTagFrequency(tickers);
-	console.log(dataFreq);
 	for (var f in dataFreq) {
 	    if (dataFreq.hasOwnProperty(f)) {
 	    	tagsFrequencyDataset.push(dataFreq[f] * 10);
 	    }
 	}
 	tagsFrequencyDataset.sort();
-	console.log(tagsFrequencyDataset);
 	drawPie(portfolioWeightsDataset);
 	drawBarChart(tagsFrequencyDataset);
-
-
 
 	 /* chart list */
 	 for(var i = 0; i < tickers.length; i++) {
 	 	var weight = "weight_" + tickers[i];
 	 	$("#companyList").append("<tr><td class='ticker'>"+tickers[i]+"</td><td><input class='weight' type='text' name='" + tickers[i] + "' /></td></tr>");
-	 	
-	 
+
+
 	 }
 
 	 $('.weight').keyup(function(e) {
-	 		for(var x = 0; x < dataset.length; x++) {
-	 			var temp = dataset[x];
+	 		for(var x = 0; x < portfolioWeightsDataset.length; x++) {
+	 			var temp = portfolioWeightsDataset[x];
 	 			console.log($(this).attr('name'));
 	 			if(temp.label === $(this).attr('name')) {
 	 				console.log("here");
@@ -47,7 +42,7 @@ $(function() {
 	 		}
 
     		$("#tagList").html($(this).val());
-    		drawPie(dataset)
+    		drawPie(portfolioWeightsDataset)
 	 	});
 
 
@@ -63,77 +58,96 @@ function cartOnClick(){
 }
 
 function drawPie(dataset){
-	d3.select("svg").remove();
+	d3.select("#listPie").select("svg").remove();
+  var data = []
+  var sum = d3.sum(dataset, function(d){ return d.count; })
+  for(var each in dataset){
+    var tmp = {'name': dataset[each].label, 'value': dataset[each].count, 'percentage': dataset[each].count/sum}
+    data.push(tmp)
+  }
 		/* companies pie chart  */
-	var width = 360;
-	var height = 360;
-	var radius = Math.min(width, height) / 2;
-	var color = d3.scaleOrdinal(d3.schemeCategory20b);
-	var legendRectSize = 18;
-	var legendSpacing = 4;
-	//var color = d3.scaleOrdinal().range(['#A60F2B', '#648C85', '#B3F2C9', '#528C18', '#C3F25C']);
-	var svg = d3.select('#listPie').append("svg")
-    .attr("width", '100%')
-    .attr("height", '100%')
-    .attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
-    .attr('preserveAspectRatio','xMinYMin')
-    .append("g")
-    .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
 
-	
+	var width = $('#listPie').width();
+	var height = $('#listPie').height();
 
-	var arc = d3.arc()
-	  .innerRadius(0)
-	  .outerRadius(radius);
-
-	var pie = d3.pie()
-	  .value(function(d) { return d.count; })
-	  .sort(null);
-	
-	var path = svg.selectAll('path')
-	  .data(pie(dataset))
-	  .enter()
-	  .append('path')
-	  .attr('d', arc)
-	  .attr('fill', function(d, i) {
-	    return color(d.data.label);
-	  });
-
+  var donut = donutChart()
+      .width(width)
+      .height(height)
+      .cornerRadius(3) // sets how rounded the corners are on each slice
+      .padAngle(0.015) // effectively dictates the gap between slices
+      .variable('percentage')
+      .category('name');
+    d3.select('#listPie')
+        .datum(data) // bind data to the div
+        .call(donut); // draw chart in div
 }
+// DRAWING
 
 function drawBarChart(dataArray) {
+  var data = [];
+  for(var i = 0; i < dataArray.length; i++){
+    var tmp = {"tag": i, "freq": dataArray[i]};
+    data.push(tmp);
+  }
+  console.log(data)
+  d3.select("#tagChart").select("svg").remove();
+  var svg = d3.select("#tagChart").append("svg")
+            .attr("height", height)
+            .attr("width", width);
 
+  var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    x = d3.scaleBand().padding(0.1),
+    y = d3.scaleLinear();
 
-// Create variable for the SVG
-var svg = d3.select("#tagChart").append("svg")
-          .attr("height","100%")
-          .attr("width","100%");
+    x.domain(data.map(function (d) { return d.tag; }));
+    y.domain([0, d3.max(data, function (d) { return d.freq; })]);
 
-// Select, append to SVG, and add attributes to rectangles for bar chart
-svg.selectAll("rect")
-    .data(dataArray)
+  var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  g.append("g")
+    .attr("class", "axis axis--x");
+
+  g.append("g")
+    .attr("class", "axis axis--y");
+
+  g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Frequency");
+
+  var width = $('#tagChart').width() - margin.left - margin.right;
+  var height = $('#tagChart').height() - margin.top - margin.bottom;
+
+  x.rangeRound([0, width]);
+  y.rangeRound([height, 0]);
+
+  g.select(".axis--x")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  g.select(".axis--y")
+    .call(d3.axisLeft(y).ticks(10, "%"));
+
+  var bars = g.selectAll(".bar")
+    .data(data);
+
+  // ENTER
+  bars
     .enter().append("rect")
-          .attr("class", "bar")
-          .attr("height", function(d, i) {return (d * 10)})
-          .attr("width","40")
-          .attr("x", function(d, i) {return (i * 60) + 25})
-          .attr("y", function(d, i) {return 400 - (d * 10)});
+    .attr("class", "bar")
+    .attr("x", function (d) { return x(d.tag); })
+    .attr("y", function (d) { return y(d.freq); })
+    .attr("width", x.bandwidth())
+    .attr("height", function (d) { return height - y(d.freq); });
 
-// Select, append to SVG, and add attributes to text
-svg.selectAll("text")
-    .data(dataArray)
-    .enter().append("text")
-    .text(function(d) {return d})
-           .attr("class", "text")
-           .attr("x", function(d, i) {return (i * 60) + 36})
-           .attr("y", function(d, i) {return 415 - (d * 10)});
-
-
+  // UPDATE
+  bars.attr("x", function (d) { return x(d.tag); })
+    .attr("y", function (d) { return y(d.freq); })
+    .attr("width", x.bandwidth())
+    .attr("height", function (d) { return height - y(d.freq); });
 }
-
-
-
-
-
 
 document.getElementById("right-brand").style.cursor = "pointer";
